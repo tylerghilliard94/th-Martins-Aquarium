17f02f9655eb51984cf8ff04550d7dab6fa1523d
Tyler Hilliard <tylerghilliard@gmail.com>
2020-06-03 10:57:13 -0500

    Sorting system

--- scripts/Fish/fishList.js
+++ scripts/Fish/fishList.js
@@ -1,10 +1,40 @@
-const fishList = () => {
 
-    for (const currentFishObject of martinFishCollection) {
-        const fishHTML = fishConverter(currentFishObject)
-    
+const showHolyFish = () => {
+    const fishObjectsArray = mostHolyFish()
+
+    for (const fishObject of fishObjectsArray) {
+        const fishHTMLRepresentation = fishConverter(fishObject)
+        
         const fishArticleElement = document.querySelector(".fishList")
-        fishArticleElement.innerHTML += fishHTML
+        fishArticleElement.innerHTML += fishHTMLRepresentation
     }
+}
+
+const showSoldierFish = () => {
+    const fishObjectsArray = soldierFish()
+
+    for (const fishObject of fishObjectsArray) {
+        const fishHTMLRepresentation = fishConverter(fishObject)
+        
+        const fishArticleElement = document.querySelector(".fishList")
+        fishArticleElement.innerHTML += fishHTMLRepresentation
+    }
+}
+
+const showNormalFish = () => {
+    const fishObjectsArray = normalFish()
+
+    for (const fishObject of fishObjectsArray) {
+        const fishHTMLRepresentation = fishConverter(fishObject)
+        
+        const fishArticleElement = document.querySelector(".fishList")
+        fishArticleElement.innerHTML += fishHTMLRepresentation
+    }
+}
+
+const fishList = () => {
+    showHolyFish();
+    showSoldierFish();
+    showNormalFish();
 }
 
--- scripts/fishData.js
+++ scripts/fishData.js
@@ -2,7 +2,7 @@ const martinFishCollection = [
     {
         name: "Angus",
         species: "Parrot Fish",
-        length: "8 cm",
+        length: 21,
         location: "Sea of Google",
         food: "Popcorn",
         picture: "https://i.ytimg.com/vi/y6tdGVTgmm4/maxresdefault.jpg"
@@ -11,7 +11,7 @@ const martinFishCollection = [
     {
         name: "Beth",
         species:"Puffer Fish",
-        length: "15 cm",
+        length: 22,
         location: "Sea of Cheddar",
         food: "Cheetos",
         picture: "https://rangerrick.org/wp-content/uploads/2018/04/RR-Pufferfish-Sept-2016.jpg"
@@ -20,7 +20,7 @@ const martinFishCollection = [
     {
         name: "Beauregard",
         species: "Lion Fish",
-        length: "12 cm",
+        length: 18,
         location: "Sea of Bing",
         food: "Pain",
         picture: "https://blueocean.net/wp-content/uploads/2017/03/florida-keys-treashres.jpg"
@@ -29,7 +29,7 @@ const martinFishCollection = [
     {
         name: "Rebecca",
         species: "Stargazer",
-        length: "14cm",
+        length: 13,
         location: "Sea of Cthulu",
         food: "Those Tasty Stars",
         picture: "https://www.saltstrong.com/wp-content/uploads/shutterstock_129741632.jpg"
@@ -38,7 +38,7 @@ const martinFishCollection = [
     {
         name: "Harry",
         species: "Hagfish",
-        length: "20 cm",
+        length: 20,
         location: "Sea of Hogwarts",
         food: "Boggarts",
         picture: "http://o.aolcdn.com/dims-shared/dims3/GLOB/crop/5208x2928+0+190/resize/800x450!/format/jpg/quality/85/http://o.aolcdn.com/hss/storage/midas/92d7f0f0babf7f031835fd5c1e55e8a9/205478081/pacific-hagfish-picture-id128615783"
@@ -47,7 +47,7 @@ const martinFishCollection = [
     {
         name: "Gertrude",
         species: "Barreleye Fish",
-        length: "40cm",
+        length: 33,
         location: "Sea of Yahoo",
         food: "Jellies",
         picture: "https://i.redditmedia.com/WsRLR2BteGJRPKmYynb93j4z5qCbjtMsD4qvkqG_QP4.jpg?w=1024&s=164a993dbc48debba5cb14ca0f5e22af"
@@ -56,7 +56,7 @@ const martinFishCollection = [
     {
         name: "Betsy",
         species: "Flowerhorn",
-        length: "10cm",
+        length: 7,
         location: "Sea of Wolfram",
         food: "Flowers",
         picture: "http://2.bp.blogspot.com/_OG7SzMSq2N0/SxBJkb-CzDI/AAAAAAAAAa0/pvIk-ByEOzg/s1600/fh.jpg"
@@ -65,7 +65,7 @@ const martinFishCollection = [
     {
         name: "Reginald",
         species: "Stonefish",
-        length: "17 cm",
+        length: 24,
         location: "Sea of the Rock",
         food: "Whatever the Rock is cookin",
         picture: "https://haydensanimalfacts.files.wordpress.com/2015/05/reef-stonefish.jpg"
@@ -74,9 +74,47 @@ const martinFishCollection = [
     {
         name: "Ross",
         species: "Goldfish",
-        length: "6cm",
+        length: 6,
         location: "Sea of Friends",
         food: "Coffee",
         picture: "https://www.thisiscolossal.com/wp-content/uploads/2019/06/tsubaki-12.jpg"
     },
 ]
+
+
+// 3, 6, 9, 12, etc... fish
+const mostHolyFish = () => {
+    const holyFish = []
+    for (const amount of martinFishCollection) {
+        if (amount.length % 3 === 0) {
+            holyFish.push(amount)
+        }
+    }
+
+    return holyFish
+}
+
+// 5, 10, 15, 20, 25, etc... fish
+const soldierFish = () => {
+   
+    const soldiers = []
+    for (const amount of martinFishCollection) {
+        if (amount.length % 5 === 0) {
+            soldiers.push(amount)
+        }
+    }
+    return soldiers
+}
+
+// Any fish not a multiple of 3 or 5
+const normalFish = () => {
+    
+    const regularFish = []
+    for (const amount of martinFishCollection) {
+        if (amount.length % 3 != 0 && amount.length % 5 != 0) {
+            regularFish.push(amount)
+        }
+    }
+
+    return regularFish
+}
